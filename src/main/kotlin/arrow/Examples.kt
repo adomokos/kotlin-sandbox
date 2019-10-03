9600f0275dc0347a17e013bb535ecdcc9669b21f
Attila Domokos <adomokos@gmail.com>
2019-10-03 16:25:00 -0500

    Getting started on function composition examples

--- src/main/kotlin/arrow/Examples.kt
+++ src/main/kotlin/arrow/Examples.kt
@@ -3,8 +3,24 @@ package arrow
 import arrow.core.*
 import arrow.effects.runEffects
 import arrow.datatypes.runDataTypes
+import arrow.core.andThen
+
+fun compositionExample() {
+	val add5 = { i: Int -> i + 5 }
+	val multiplyBy2 = { i: Int -> i * 2 }
+	val idOdd = { x: Int -> x % 2 != 0 }
+
+	val composed: (Int) -> Option<Int> = { i: Int -> Option.just(i)
+		.filter(idOdd)
+		.map(add5.andThen(multiplyBy2))
+	}
+
+	println(composed(3))
+	println(composed(4))
+}
 
 fun runExamples() {
 	runEffects()
 	runDataTypes()
+	compositionExample()
 }
