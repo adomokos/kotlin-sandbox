3e91f507ee926417e258e641609469c5fc252412
Attila Domokos <adomokos@gmail.com>
2019-10-03 23:01:22 -0500

    Add the Calculator to the test itself

--- src/test/kotlin/samples/CalculatorSpec.kt
+++ src/test/kotlin/samples/CalculatorSpec.kt
@@ -4,6 +4,20 @@ import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
 import kotlin.test.assertEquals
 
+class Calculator {
+    fun add(a: Int, b: Int): Int {
+        return a + b
+    }
+
+	fun subtract(a: Int, b: Int): Int {
+		return a - b
+	}
+
+	fun multiply(a: Int, b: Int): Int {
+		return a * b
+	}
+}
+
 object CalculatorTest: Spek({
     describe("Calculator") {
         val calculator by memoized { Calculator() }
@@ -14,6 +28,10 @@ object CalculatorTest: Spek({
 
 		it ("can subtract two numbers") {
 			assertEquals(1, calculator.subtract(3, 2))
+		}
+
+		it ("can multiply two numbers") {
+			assertEquals(6, calculator.multiply(2, 3))
 		}
     }
 })
