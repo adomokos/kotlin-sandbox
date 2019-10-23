69722df33d9174706db78ebf04eda17586f5f857
Attila Domokos <adomokos@gmail.com>
2019-10-22 20:10:07 -0500

    Add more Functor examples

--- src/test/kotlin/arrow/FunctorSpec.kt
+++ src/test/kotlin/arrow/FunctorSpec.kt
@@ -3,11 +3,16 @@ package arrow
 import arrow.core.Either
 import arrow.core.None
 import arrow.core.Option
+import arrow.core.Right
 import arrow.core.Some
+import arrow.core.extensions.either.functor.functor
+import arrow.core.extensions.fx
 import arrow.core.extensions.option.functor.functor
 import arrow.core.none
+import arrow.fx.IO
+import arrow.fx.extensions.fx
 import io.kotlintest.shouldBe
-import io.kotlintest.specs.StringSpec
+import io.kotlintest.specs.DescribeSpec
 
 fun addOne(n: Int): Int {
     return n + 1
@@ -15,34 +20,71 @@ fun addOne(n: Int): Int {
 
 fun inc(n: Int) = n + 1
 
-class FunctorSpec : StringSpec({
-    "can map over Option" {
-        val result = Option(1).map { 1 * 2 }
-        result shouldBe Some(2)
+class FunctorSpec : DescribeSpec({
+    val optionFunctor = Option.functor()
+    val eitherFunctor = Either.functor<String>()
 
-        val noneResult = none<Int>().map { it * 2 }
-        noneResult shouldBe None
+    describe("for Option") {
+        it("can map over") {
+            val result = Option(1).map { it * 2 }
+            result shouldBe Some(2)
 
-        // Functor with Option
-        val x = Option(1).map(::inc)
-        x shouldBe Some(2)
+            val noneResult = none<Int>().map { it * 2 }
+            noneResult shouldBe None
 
-        // Functor with Either
-        val y = Either.Right(5).map(::inc)
-        y shouldBe Either.Right(6)
+            // Functor with Option
+            val x = Option(1).map(::inc)
+            x shouldBe Some(2)
+        }
+
+        it("works with Kind<F,A>#map combinator") {
+            val result = optionFunctor.run { Option(1).map { it + 1 } }
+
+            result shouldBe Some(2)
+        }
+
+        it("can lift a function into the Functor context") {
+            val lifted = optionFunctor.lift(::addOne)
+            lifted(Option(1)) shouldBe Some(2)
+        }
+
+        it("can extract from the Functor context with fx") {
+            val result = Option(1).map { it * 2 }
+
+            Option.fx {
+                val (calcResult) = result
+                calcResult shouldBe 2
+            }
+        }
     }
 
-    "works with Kind<F,A>#map combinator" {
-        val optionFunctor = Option.functor()
-        val result = optionFunctor.run { Option(1).map { it + 1 } }
+    describe("for Either") {
+        it("can map over") {
+            var result = Either.right(1).map { it * 2 }
+            result shouldBe Right(2)
 
-        result shouldBe Some(2)
+            var leftResult = Either.left("Will not work").map(::inc)
+            leftResult shouldBe Either.Left("Will not work")
+        }
+
+        it("works with Kind<F, A>#map combinator") {
+            val result = eitherFunctor.run { Either.right(2).map { it + 1 } }
+            result shouldBe Either.Right(3)
+        }
+
+        it("can lift a function into the Functor context") {
+            val lifted = eitherFunctor.lift(::addOne)
+            lifted(Either.right(2)) shouldBe Either.Right(3)
+        }
     }
 
-    "can lift a function into the Functor context" {
-        val optionFunctor = Option.functor()
-        // val addOne = {n: Int -> n + 1}
-        val lifted = optionFunctor.lift(::addOne)
-        lifted(Option(1)) shouldBe Some(2)
+    describe("for IO") {
+        it("can map over") {
+            var ioCalc = IO<Int> { 1 }.map { it * 2 }
+            IO.fx {
+                val (result) = ioCalc
+                result shouldBe Right(2)
+            }
+        }
     }
 })
