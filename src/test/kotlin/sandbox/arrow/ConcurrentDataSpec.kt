40055e02de93199547446290fe38217ffb223158
Attila Domokos <adomokos@gmail.com>
2019-12-12 12:17:02 -0600

    Add the parTraverse example to the specs

--- src/test/kotlin/sandbox/arrow/ConcurrentDataSpec.kt
+++ src/test/kotlin/sandbox/arrow/ConcurrentDataSpec.kt
@@ -12,7 +12,7 @@ data class UserInfo(
     val lastName: String
 )
 
-suspend fun findUserInfo(i: Int): IO<UserInfo> =
+fun findUserInfo(i: Int): IO<UserInfo> =
     // Simulate DB calls
     IO.fx {
         if (i == 1) {
@@ -40,6 +40,15 @@ val program2 = IO.fx {
     result
 }
 
+// parTraverse
+val program3 = IO.fx {
+    val result = !
+        listOf(1, 2).parTraverse { i ->
+            findUserInfo(i)
+        }
+    result
+}
+
 class ConcurrentDataSpec : DescribeSpec({
     describe("Concurrent Data Manipulation") {
         it("can fetch data concurrently with forked IO") {
@@ -54,6 +63,15 @@ class ConcurrentDataSpec : DescribeSpec({
             IO.fx {
                 val (users) = program2
                 val firstNames = listOf(users.a.firstName, users.b.lastName)
+
+                firstNames.sorted() shouldBe listOf("John", "Paul")
+            }
+        }
+
+        it("can fetch data with parTraverse") {
+            IO.fx {
+                val (users) = program3
+                val firstNames = users.map { it.firstName }
 
                 firstNames.sorted() shouldBe listOf("John", "Paul")
             }
