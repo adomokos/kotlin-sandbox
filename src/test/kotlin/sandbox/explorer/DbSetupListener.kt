f55333baf2cba9717964104b0a31307a72d98496
Attila Domokos <adomokos@gmail.com>
2020-01-12 11:45:04 -0600

    Change import orders to fix the build

--- src/test/kotlin/sandbox/explorer/DbSetupListener.kt
+++ src/test/kotlin/sandbox/explorer/DbSetupListener.kt
@@ -2,10 +2,10 @@ package sandbox.explorer
 
 import io.kotlintest.TestCase
 import io.kotlintest.extensions.TestListener
-import org.jetbrains.exposed.sql.Database
-import org.jetbrains.exposed.sql.transactions.TransactionManager
 import java.io.File
 import java.sql.Connection
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.transactions.TransactionManager
 
 object DbSetupListener : TestListener {
     override fun beforeTest(testCase: TestCase) {
