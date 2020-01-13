b35ef5181238f8ae2d1d13eb292388aa12ea2700
Attila Domokos <adomokos@gmail.com>
2020-01-12 20:14:26 -0600

    Add a Reader example spec

---
+++ src/test/kotlin/sandbox/arrow/ReaderSpec.kt
@@ -0,0 +1,56 @@
+package sandbox.arrow
+
+import arrow.core.Either
+import arrow.core.Left
+import arrow.core.Right
+import arrow.core.flatMap
+import arrow.core.value
+import arrow.fx.IO
+import arrow.fx.extensions.fx
+import arrow.fx.handleError
+import arrow.mtl.Reader
+import arrow.mtl.ReaderApi
+import arrow.mtl.fix
+import arrow.mtl.map
+import io.kotlintest.shouldBe
+import io.kotlintest.specs.StringSpec
+
+class ReaderSpec : StringSpec() {
+    object AppError
+
+    data class GetAppContext(
+        val number: String
+    )
+
+    private fun one(): IO<Either<AppError, Int>> =
+        IO { Right(1) }
+    private fun two(inputString: String): IO<Either<AppError, String>> =
+        IO { Right(inputString) }
+    private fun toInt(str: String): IO<Either<AppError, Int>> =
+        IO { Right(str.toInt()) }.handleError { Left(AppError) }
+
+    private fun myApp(): Reader<GetAppContext, IO<Either<AppError, Int>>> =
+        ReaderApi.ask<GetAppContext>().map { ctx ->
+            IO.fx {
+                val oneResult = !one()
+                val parsedTwo = !toInt(ctx.number)
+
+                // oneResult.combineK(parsedTwo)
+                oneResult.flatMap { x ->
+                    parsedTwo.map { y ->
+                        x + y
+                    }
+                }
+            }
+        }.fix()
+
+    init {
+        "can pull data from the Reader Context" {
+            val appContext = GetAppContext(number = "3")
+            val appEffect = myApp().run(appContext).value()
+
+            val result = appEffect.unsafeRunSync()
+            result shouldBe Right(4)
+        }
+    }
+}
