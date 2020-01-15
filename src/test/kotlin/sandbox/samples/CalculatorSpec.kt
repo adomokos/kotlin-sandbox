973db332ff383287ff1cfab9bcedcb22f61f0629
Attila Domokos <adomokos@gmail.com>
2020-01-14 22:10:56 -0600

    Clean up old calculator test

--- src/test/kotlin/sandbox/samples/CalculatorSpec.kt
+++ src/test/kotlin/sandbox/samples/CalculatorSpec.kt
@@ -26,25 +26,7 @@ class CalculatorSpec : StringSpec({
         result shouldBe 4
     }
 
-    "f:startsWith should test for a prefix" {
+    "startsWith should test for a prefix" {
         "world" should startWith("wor")
     }
 })
-
-// object CalculatorTest: Spek({
-    // describe("Calculator") {
-        // val calculator by memoized { Calculator() }
-
-        // it ("can add two numbers") {
-            // assertEquals(3, calculator.add(1, 2))
-        // }
-
-        // it ("can subtract two numbers") {
-            // assertEquals(1, calculator.subtract(3, 2))
-        // }
-
-        // it ("can multiply two numbers") {
-            // assertEquals(6, calculator.multiply(2, 3))
-        // }
-    // }
-// })
