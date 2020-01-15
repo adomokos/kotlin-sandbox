68c23e854d84c9a41ecac0f824e993d39d8ef145
Attila Domokos <adomokos@gmail.com>
2020-01-14 20:13:36 -0600

    Typealias example

--- src/test/kotlin/sandbox/arrow/EitherTSpec.kt
+++ src/test/kotlin/sandbox/arrow/EitherTSpec.kt
@@ -13,9 +13,9 @@ import arrow.mtl.fix
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.StringSpec
 
-class EitherTSpec : StringSpec() {
-    object AppError
+typealias EitherTIO<A> = EitherT<ForIO, AppError, A>
 
+class EitherTSpec : StringSpec() {
     private fun one(): EitherT<ForIO, AppError, Int> =
         EitherT(IO { Right(1) })
     private fun two(inputString: String): EitherT<ForIO, AppError, Int> =
@@ -24,19 +24,42 @@ class EitherTSpec : StringSpec() {
         EitherT(IO { Right(str.toInt()) }.handleError { Left(AppError) })
 
     // https://stackoverflow.com/a/53747937
-    fun result(inputString: String): EitherT<ForIO, AppError, Int> =
+    private fun result(inputString: String): EitherT<ForIO, AppError, Int> =
         EitherT.monad<ForIO, AppError>(IO.monad()).fx.monad {
             val oneInt = ! one()
             val twoInt = ! two(inputString)
             oneInt + twoInt
         }.fix()
 
+    // Using typealias
+    private fun oneT(): EitherTIO<Int> =
+        EitherTIO(IO { Right(1) })
+    private fun twoT(inputString: String): EitherTIO<Int> =
+        toIntT(inputString)
+    private fun toIntT(str: String): EitherTIO<Int> =
+        EitherTIO(IO { Right(str.toInt()) }.handleError { Left(AppError) })
+
+    private fun resultT(inputString: String): EitherTIO<Int> =
+        EitherT.monad<ForIO, AppError>(IO.monad()).fx.monad {
+            val oneInt = ! oneT()
+            val twoInt = ! twoT(inputString)
+            oneInt + twoInt
+        }.fix()
+
     init {
-        "can combine operations with IO<Either>" {
+        "can combine operations with EitherIO" {
             val rightResult = result("2").value().fix().unsafeRunSync()
             rightResult shouldBe Right(3)
 
             val leftResult = result("three").value().fix().unsafeRunSync()
+            leftResult shouldBe Left(AppError)
+        }
+
+        "can combine operations with EitherTIO - a type alias" {
+            val rightResult = resultT("2").value().fix().unsafeRunSync()
+            rightResult shouldBe Right(3)
+
+            val leftResult = resultT("three").value().fix().unsafeRunSync()
             leftResult shouldBe Left(AppError)
         }
     }
