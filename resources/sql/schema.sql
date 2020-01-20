51505abba3d5fd969d113e70e035953eacdc872c
Attila Domokos <adomokos@gmail.com>
2020-01-20 10:39:04 -0600

    Use combined unique indexes - a user can have multiple GitHub user accounts

--- resources/sql/schema.sql
+++ resources/sql/schema.sql
@@ -1,11 +1,12 @@
 CREATE TABLE people (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  email varchar(255) NOT NULL UNIQUE,
+  email varchar(255) NOT NULL,
   firstname varchar(255) NOT NULL,
   lastname varchar(255) NOT NULL,
-  git_hub_username varchar(255) NOT NULL UNIQUE,
+  git_hub_username varchar(255) NOT NULL,
   rating integer(255) DEFAULT 0,
-  created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  unique (email, git_hub_username)
 );
 
 CREATE TABLE git_hub_metrics (
