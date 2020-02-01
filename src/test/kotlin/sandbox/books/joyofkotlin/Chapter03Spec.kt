0964ff55c863ba4eb13ce206980a4b4dfff6ca06
Attila Domokos <adomokos@gmail.com>
2020-01-31 20:18:10 -0600

    Add folding example

--- src/test/kotlin/sandbox/books/joyofkotlin/Chapter03Spec.kt
+++ src/test/kotlin/sandbox/books/joyofkotlin/Chapter03Spec.kt
@@ -21,15 +21,39 @@ class Chapter03Spec : StringSpec() {
 
         operator fun times(num: Int) =
             Price(this.value * num)
+
+        companion object {
+            val identity = Price(0.0)
+
+            operator fun invoke(value: Double) =
+                if (value > 0)
+                    Price(value)
+                else
+                    throw IllegalArgumentException(
+                        "Price must be positive or null"
+                    )
+        }
     }
 
     data class Weight(val value: Double) {
-        operator fun plus(weight: Price) =
-            Price(this.value + weight.value)
-    }
+        operator fun plus(weight: Weight) =
+            Weight(this.value + weight.value)
 
-    val zeroPrice = Price(0.0)
-    val zeroWeight = Weight(0.0)
+        operator fun times(num: Int) =
+            Weight(this.value * num)
+
+        companion object {
+            val identity = Weight(0.0)
+
+            operator fun invoke(value: Double) =
+                if (value > 0)
+                    Weight(value)
+                else
+                    throw IllegalArgumentException(
+                        "Price must be positive or null"
+                    )
+        }
+    }
     // val priceAddition = { x, y -> x + y }
 
     data class EnhancedProduct(
@@ -37,6 +61,11 @@ class Chapter03Spec : StringSpec() {
         val price: Price,
         val weight: Weight
     )
+
+    data class EnhancedOrderLine(val product: EnhancedProduct, val count: Int) {
+        fun weight() = product.weight * count
+        fun amount() = product.price * count
+    }
 
     init {
         "calculates total price and weight incorrectly" {
@@ -72,6 +101,22 @@ class Chapter03Spec : StringSpec() {
                 EnhancedProduct("Tooth brush",
                                 Price(3.5),
                                 Weight(0.3))
+
+            val orderLines = listOf(
+                EnhancedOrderLine(toothPaste, 2),
+                EnhancedOrderLine(toothBrush, 3)
+            )
+
+            val weight: Weight = orderLines.fold(Weight.identity) {
+                    a, b -> a + b.weight()
+            }
+
+            val price: Price = orderLines.fold(Price.identity) {
+                    a, b -> a + b.amount()
+            }
+
+            weight shouldBe Weight(1.9)
+            price shouldBe Price(13.5)
         }
     }
 }
