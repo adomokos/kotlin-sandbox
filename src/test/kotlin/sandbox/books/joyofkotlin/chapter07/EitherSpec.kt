61b24a00eab5ddce396fdbf36af737a51905dd29
Attila Domokos <adomokos@gmail.com>
2020-03-29 15:51:11 -0500

    Build map (a.k.a fmap) for Either

--- src/test/kotlin/sandbox/books/joyofkotlin/chapter07/EitherSpec.kt
+++ src/test/kotlin/sandbox/books/joyofkotlin/chapter07/EitherSpec.kt
@@ -5,12 +5,16 @@ import io.kotest.matchers.shouldBe
 import sandbox.books.joyofkotlin.chapter05.List as LList
 
 sealed class Either<out A, out B> {
+    abstract fun <C> map(f: (B) -> C): Either<A, C>
+
     internal data class Left<out A, out B>(val value: A) : Either<A, B>() {
         override fun toString(): String = "Left($value)"
+        override fun <C> map(f: (B) -> C): Either<A, C> = Left(value)
     }
 
     internal data class Right<out A, out B>(private val value: B) : Either<A, B>() {
         override fun toString(): String = "Right($value)"
+        override fun <C> map(f: (B) -> C): Either<A, C> = Right(f(value))
     }
 
     companion object {
@@ -33,6 +37,14 @@ class EitherSpec : StringSpec() {
             val list = LList(3, 5, 8, 4, 1)
 
             max(list) shouldBe Either.right<String, Int>(8)
+        }
+
+        "can work with map over its fields" {
+            val leftValue = Either.left<String, Int>("error")
+            leftValue.map { x: Int -> x * 2 } shouldBe Either.left<String, Int>("error")
+
+            val rightValue = Either.right<String, Int>(2)
+            rightValue.map { x: Int -> x * 3 } shouldBe Either.right<String, Int>(6)
         }
     }
 }
