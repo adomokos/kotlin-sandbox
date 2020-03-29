aff2203b0378730ff93ea277089beb1af392e89f
Attila Domokos <adomokos@gmail.com>
2020-03-28 21:41:08 -0500

    Expose List<A> for further use

--- src/test/kotlin/sandbox/books/joyofkotlin/chapter05/LinkedListSpec.kt
+++ src/test/kotlin/sandbox/books/joyofkotlin/chapter05/LinkedListSpec.kt
@@ -1,122 +1,120 @@
 package sandbox.books.joyofkotlin.chapter05
 
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import java.lang.IllegalArgumentException
+
 /*
 Collections can be classified as:
   * linear collection (like list, there is connection between elements)
   * associative collection (no connection between elements, like maps or sets)
   * graph collection (trees, like binary tree)
-
-
  */
 
-import io.kotest.core.spec.style.StringSpec
-import io.kotest.matchers.shouldBe
-import java.lang.IllegalArgumentException
+// Sealed classes are implicitly abstract and their constructor
+// is implicitly private
+sealed class List<A> {
+    abstract fun isEmpty(): Boolean
+    abstract fun setHead(a: A): List<A>
+    abstract fun drop(n: Int): List<A>
+    abstract fun dropWhile(p: (A) -> Boolean): List<A>
+    abstract fun reverse(): List<A>
 
-class LinkedListSpec : StringSpec() {
-    // Sealed classes are implicitly abstract and their constructor
-    // is implicitly private
-    sealed class List<A> {
-        abstract fun isEmpty(): Boolean
-        abstract fun setHead(a: A): List<A>
-        abstract fun drop(n: Int): List<A>
-        abstract fun dropWhile(p: (A) -> Boolean): List<A>
-        abstract fun reverse(): List<A>
-
-        object Nil : List<Nothing>() {
-            override fun isEmpty() = true
-            override fun setHead(a: Nothing): List<Nothing> =
-                throw IllegalArgumentException("setHead called on an empty list")
-            override fun drop(n: Int) =
-                drop(this, n)
-            override fun toString(): String = "[NIL]"
-            override fun dropWhile(p: (Nothing) -> Boolean): List<Nothing> =
-                dropWhile(this, p)
-            override fun reverse(): List<Nothing> =
-                reverse(
-                    invoke(),
-                    this
-                )
-        }
-
-        fun cons(a: A): List<A> =
-            Cons(a, this)
-
-        class Cons<A>(
-            internal val head: A,
-            internal val tail: List<A>
-        ) : List<A>() {
-            override fun isEmpty() = false
-
-            override fun toString(): String = "[${toString("", this)}NIL]"
-
-            override fun setHead(a: A): List<A> = tail.cons(a)
-
-            override fun drop(n: Int): List<A> =
-                drop(this, n)
-
-            override fun dropWhile(p: (A) -> Boolean): List<A> =
-                dropWhile(this, p)
-
-            override fun reverse(): List<A> =
-                reverse(
-                    invoke(),
-                    this
-                )
-
-            private tailrec fun toString(acc: String, list: List<A>): String =
-                when (list) {
-                    Nil -> acc
-                    is Cons -> toString("$acc${list.head}, ", list.tail)
-                }
-        }
-
-        companion object {
-            @Suppress("UNCHECKED_CAST")
-            operator fun <A> invoke(vararg az: A): List<A> =
-                az.foldRight(Nil as List<A>) {
-                    a: A, list: List<A> ->
-                    Cons(a, list)
-                }
-
-            tailrec fun <A> drop(list: List<A>, n: Int): List<A> =
-                when (list) {
-                    Nil -> list
-                    is Cons -> if (n <= 0) list else drop(
-                        list.tail,
-                        n - 1
-                    )
-                }
-
-            tailrec fun <A> dropWhile(list: List<A>, p: (A) -> Boolean): List<A> =
-                when (list) {
-                    Nil -> list
-                    is Cons -> if (p(list.head)) dropWhile(
-                        list.tail,
-                        p
-                    ) else list
-                }
-
-            fun <A> concat(list1: List<A>, list2: List<A>): List<A> =
-                when (list1) {
-                    Nil -> list2
-                    is Cons -> concat(
-                        list1.tail,
-                        list2
-                    ).cons(list1.head)
-                }
-
-            tailrec fun <A> reverse(acc: List<A>, list: List<A>): List<A> =
-                when (list) {
-                    Nil -> acc
-                    is Cons -> reverse(
-                        acc.cons(list.head),
-                        list.tail
-                    )
-                }
-        }
+    object Nil : List<Nothing>() {
+        override fun isEmpty() = true
+        override fun setHead(a: Nothing): List<Nothing> =
+            throw IllegalArgumentException("setHead called on an empty list")
+        override fun drop(n: Int) =
+            drop(this, n)
+        override fun toString(): String = "[NIL]"
+        override fun dropWhile(p: (Nothing) -> Boolean): List<Nothing> =
+            dropWhile(this, p)
+        override fun reverse(): List<Nothing> =
+            reverse(
+                invoke(),
+                this
+            )
     }
 
+    fun cons(a: A): List<A> =
+        Cons(a, this)
+
+    class Cons<A>(
+        internal val head: A,
+        internal val tail: List<A>
+    ) : List<A>() {
+        override fun isEmpty() = false
+
+        override fun toString(): String = "[${toString("", this)}NIL]"
+
+        override fun setHead(a: A): List<A> = tail.cons(a)
+
+        override fun drop(n: Int): List<A> =
+            drop(this, n)
+
+        override fun dropWhile(p: (A) -> Boolean): List<A> =
+            dropWhile(this, p)
+
+        override fun reverse(): List<A> =
+            reverse(
+                invoke(),
+                this
+            )
+
+        private tailrec fun toString(acc: String, list: List<A>): String =
+            when (list) {
+                Nil -> acc
+                is Cons -> toString("$acc${list.head}, ", list.tail)
+            }
+    }
+
+    companion object {
+        @Suppress("UNCHECKED_CAST")
+        operator fun <A> invoke(vararg az: A): List<A> =
+            az.foldRight(Nil as List<A>) {
+                    a: A, list: List<A> ->
+                Cons(a, list)
+            }
+
+        tailrec fun <A> drop(list: List<A>, n: Int): List<A> =
+            when (list) {
+                Nil -> list
+                is Cons -> if (n <= 0) list else drop(
+                    list.tail,
+                    n - 1
+                )
+            }
+
+        tailrec fun <A> dropWhile(list: List<A>, p: (A) -> Boolean): List<A> =
+            when (list) {
+                Nil -> list
+                is Cons -> if (p(list.head)) dropWhile(
+                    list.tail,
+                    p
+                ) else list
+            }
+
+        fun <A> concat(list1: List<A>, list2: List<A>): List<A> =
+            when (list1) {
+                Nil -> list2
+                is Cons -> concat(
+                    list1.tail,
+                    list2
+                ).cons(list1.head)
+            }
+
+        tailrec fun <A> reverse(acc: List<A>, list: List<A>): List<A> =
+            when (list) {
+                Nil -> acc
+                is Cons -> reverse(
+                    acc.cons(list.head),
+                    list.tail
+                )
+            }
+    }
+}
+
+class LinkedListSpec : StringSpec() {
     private fun product(ints: List<Int>): Int =
         when (ints) {
             is List.Cons -> ints.head * product(ints.tail)
