74688d46fa2b9658f7ea2f8e262d755ef46fc480
Attila Domokos <adomokos@gmail.com>
2020-03-29 17:25:01 -0500

    Introducing the Result type

--- src/test/kotlin/sandbox/books/joyofkotlin/chapter07/EitherSpec.kt
+++ src/test/kotlin/sandbox/books/joyofkotlin/chapter07/EitherSpec.kt
@@ -2,28 +2,81 @@ package sandbox.books.joyofkotlin.chapter07
 
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
+import java.io.Serializable
+import java.lang.IllegalStateException
+import java.lang.NullPointerException
 import sandbox.books.joyofkotlin.chapter05.List as LList
 
 sealed class Either<out A, out B> {
     abstract fun <C> map(f: (B) -> C): Either<A, C>
     abstract fun <C> flatMap(f: (B) -> Either<@UnsafeVariance A, C>): Either<A, C>
+    abstract fun getOrElse(defaultValue: () -> @UnsafeVariance B): B
+    abstract fun orElse(defaultValue: () -> Either<@UnsafeVariance A, @UnsafeVariance B>): Either<A, B>
 
     internal data class Left<out A, out B>(val value: A) : Either<A, B>() {
         override fun toString(): String = "Left($value)"
         override fun <C> map(f: (B) -> C): Either<A, C> = Left(value)
         override fun <C> flatMap(f: (B) -> Either<@UnsafeVariance A, C>): Either<A, C> =
             Left(value)
+        override fun getOrElse(defaultValue: () -> @UnsafeVariance B): B = defaultValue()
+        override fun orElse(defaultValue: () -> Either<@UnsafeVariance A, @UnsafeVariance B>) = defaultValue()
     }
 
     internal data class Right<out A, out B>(private val value: B) : Either<A, B>() {
         override fun toString(): String = "Right($value)"
         override fun <C> map(f: (B) -> C): Either<A, C> = Right(f(value))
         override fun <C> flatMap(f: (B) -> Either<@UnsafeVariance A, C>): Either<A, C> = f(value)
+        override fun getOrElse(defaultValue: () -> @UnsafeVariance B): B = value
+        override fun orElse(defaultValue: () -> Either<@UnsafeVariance A, @UnsafeVariance B>) = this
     }
 
     companion object {
         fun <A, B> left(value: A): Either<A, B> = Left(value)
         fun <A, B> right(value: B): Either<A, B> = Right(value)
+    }
+}
+
+sealed class Result<out A> : Serializable {
+    abstract fun <B> map(f: (A) -> B): Result<B>
+    abstract fun <B> flatMap(f: (A) -> Result<B>): Result<B>
+
+    internal data class Failure<out A>(internal val exception: RuntimeException) : Result<A>() {
+        override fun toString(): String = "Failure(${exception.message})"
+        override fun <B> map(f: (A) -> B): Result<B> = Failure(exception)
+        override fun <B> flatMap(f: (A) -> Result<B>): Result<B> = Failure(exception)
+    }
+
+    internal data class Success<out A>(internal val value: A) : Result<A>() {
+        override fun toString(): String = "Success($value)"
+        override fun <B> map(f: (A) -> B): Result<B> =
+            try {
+                Success(f(value))
+            } catch (e: RuntimeException) {
+                Failure(e)
+            } catch (e: Exception) {
+                Failure(RuntimeException(e))
+            }
+
+        override fun <B> flatMap(f: (A) -> Result<B>): Result<B> =
+            try {
+                f(value)
+            } catch (e: RuntimeException) {
+                Failure(e)
+            } catch (e: Exception) {
+                Failure(RuntimeException(e))
+            }
+    }
+
+    companion object {
+        operator fun <A> invoke(a: A? = null): Result<A> =
+            when (a) {
+                null -> Failure(NullPointerException())
+                else -> Success(a)
+            }
+
+        fun <A> failure(message: String): Result<A> = Failure(IllegalStateException(message))
+        fun <A> failure(exception: RuntimeException): Result<A> = Failure(exception)
+        fun <A> failure(exception: Exception): Result<A> = Failure(IllegalStateException(exception))
     }
 }
 
@@ -58,6 +111,22 @@ class EitherSpec : StringSpec() {
 
             val rightValue = Either.right<String, Int>(2)
             rightValue.flatMap { x: Int -> doubleFn(x) } shouldBe Either.right<String, Int>(4)
+        }
+
+        "can extract value with getOrElse" {
+            val leftValue = Either.left<String, Int>("error")
+            leftValue.getOrElse { 8 } shouldBe 8
+
+            val rightValue = Either.right<String, Int>(2)
+            rightValue.getOrElse { 8 } shouldBe 2
+        }
+
+        "computation can return a Result object" {
+            val result = Result(5)
+            result.map { it * 3 } shouldBe Result(15)
+
+            val failure = Result.failure<Int>("error")
+            failure.map { it * 3 }.toString() shouldBe "Failure(error)"
         }
     }
 }
